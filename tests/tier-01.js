bac96dd8cbe922ae8018f2599d9e4bfa74a4a201
Collin Miller <collintmiller@gmail.com>
2018-05-18 17:50:23 -0500

    teir-01 async/await

--- tests/tier-01.js
+++ tests/tier-01.js
@@ -25,34 +25,36 @@ import { setCampuses } from '../client/redux/actions';
 import reducer from '../client/redux/reducer';
 
 describe('Tier One', () => {
-  before(() => db.sync({ force: true }));
-  after(() => db.sync({ force: true }));
+  before(async () => await db.sync({ force: true }));
+  after(async () => await db.sync({ force: true }));
 
   // defined in ../server/models/Campus.js
   describe('Campus model', () => {
     describe('Validations', () => {
-      xit('requires name', () => {
+      xit('requires name', async () => {
         const campus = Campus.build();
 
-        return campus.validate()
-        .then(() => {
+        try {
+          await campus.validate()
           throw Error('validation was successful but should have failed without `name`');
-        }, err => {
+        }
+        catch (err) {
           expect(err.message).to.contain('name cannot be null');
-        });
+        }
       });
 
-      xit('requires name to not be an empty string', () => {
+      xit('requires name to not be an empty string', async () => {
         const campus = Campus.build({
           name: ''
         });
 
-        return campus.validate()
-        .then(() => {
+        try {
+          await campus.validate()
           throw Error('validation was successful but should have failed if name is an empty string');
-        }, err => {
+        } catch (err) {
           expect(err.message).to.contain('Validation error');
-        });
+          /* handle error */
+        }
       });
     });
   });
@@ -70,35 +72,29 @@ describe('Tier One', () => {
       }
     ];
 
-    before(() =>
-      Campus.bulkCreate(campusData)
-      .then(createdCampuses => {
-        storedCampuses = createdCampuses.map(campus => campus.dataValues);
-      })
-    );
+    before(async () => {
+      const createdCampuses = await Campus.bulkCreate(campusData)
+      storedCampuses = createdCampuses.map(campus => campus.dataValues);
+    });
 
     // Route for fetching all campuses
     describe('GET /api/campuses', () => {
-      xit('serves up all Campuses', () => {
-        return agent
-        .get('/api/campuses')
-        .expect(200)
-        .then(response => {
-          expect(response.body).to.have.length(2);
-          expect(response.body[0].name).to.equal(storedCampuses[0].name);
-        });
+      xit('serves up all Campuses', async () => {
+        const response = await agent
+          .get('/api/campuses')
+          .expect(200);
+        expect(response.body).to.have.length(2);
+        expect(response.body[0].name).to.equal(storedCampuses[0].name);
       });
     });
 
     // Route for fetching a single campus
     describe('GET /api/campuses/:id', () => {
-      xit('serves up a single Campus by its id', () => {
-        return agent
-        .get('/api/campuses/1')
-        .expect(200)
-        .then(response => {
-          expect(response.body.name).to.equal('Grace Hopper');
-        });
+      xit('serves up a single Campus by its id', async () => {
+        const response = await agent
+          .get('/api/campuses/1')
+          .expect(200);
+        expect(response.body.name).to.equal('Grace Hopper');
       });
     });
   });
@@ -118,16 +114,13 @@ describe('Tier One', () => {
         expect(wrapper.find('ul')).to.have.length(1);
       })
 
-      xit('renders list items for the campuses passed in as props', () => {
-
-        return Campus.bulkCreate(campuses)
-        .then(() => {
-          //we are creating the campuses in the database so the extra credit in tier-4 doesn't break this spec.
-          const wrapper = shallow(<CampusList campuses={campuses} />);
-          const listItems = wrapper.find('li');
-          expect(listItems).to.have.length(3);
-          expect(listItems.at(2).text()).to.contain('Pluto');
-        })
+      xit('renders list items for the campuses passed in as props', async () => {
+        const campuses = await Campus.bulkCreate(campuses)
+        //we are creating the campuses in the database so the extra credit in tier-4 doesn't break this spec.
+        const wrapper = shallow(<CampusList campuses={campuses} />);
+        const listItems = wrapper.find('li');
+        expect(listItems).to.have.length(3);
+        expect(listItems.at(2).text()).to.contain('Pluto');
       });
     });
 
