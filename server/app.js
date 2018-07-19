69e8d2794a36c9bb0850addd27f8f2daf572f141
Thomas Kelly <tmkelly28@gmail.com>
2018-07-19 10:57:04 -0400

    bye bye body parser

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
