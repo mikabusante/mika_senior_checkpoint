214fb0fd65077fc95cc5fd192b5c2d3f68a76c08
Karen MacPherson <karen@fullstackacademy.com>
2018-08-23 14:43:14 +0000

    update name of config file

--- server/models/_db.js
+++ server/models/_db.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const config = require('../config/config');
+const config = require('../config');
 
 module.exports = new Sequelize(config.database, config.username, config.password, config);
