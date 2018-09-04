132328041db8f8ad4294bacfb0fabf023c5c8c70
Gabriel Lebec <glebec@gmail.com>
2018-09-03 21:17:18 -0400

    docs(t2): break long form comment at 80 chars (#66)

--- tests/tier-02.js
+++ tests/tier-02.js
@@ -235,13 +235,18 @@ describe('Tier Two', () => {
   });
 
   // defined in ../utils/index.js
-  /*
-    One thing we'd like for each campus is to generate groups of students.
-    Although for now we won't be integrating this into our end user view, we'd like for you to address the functionality.
-    Write a function that will take in an array of strings, representing student names, and the size of groups to be made (number).
-    This `generateGroups` function will return an array of arrays of student groups.
-    All students that may not fit evenly into the expected length of groups will be placed in their own group.
-  */
+
+  /**
+   * One thing we'd like for each campus is to generate groups of students.
+   * Although for now we won't be integrating this into our end user view,
+   * we'd like for you to address the functionality.
+   *
+   * Write a function that will take in an array of strings, representing
+   * student names, and the size of groups to be made (number). This
+   * `generateGroups` function will return an array of arrays of student
+   * groups. All students that may not fit evenly into the expected length
+   * of groups will be placed in their own group.
+   */
 
   describe('`generateGroups` utility method', () => {
     xit('takes an array and a number (size) and returns an array', () => {
